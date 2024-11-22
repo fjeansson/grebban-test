7f709489e69e7731b9d907e39ebac01e5a3df8ba
Fredrik Jeansson <fredrik@media.se>
2024-11-22 15:01:06 +0100

    bugfix.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (Throwable $e) {
+        $exceptions->render(function (ErrorException $e) {
             return response()->json(['error' => 'The input JSON data is inconsitent.']);
         });
     })
